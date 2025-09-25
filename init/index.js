291beb9c5346a136ac48d384e1202cc0262d33e9
Gopika-solanki08 <gopikasolanki6@gmail.com>
2025-09-25 15:42:59 +0530

    Add files via upload

---
+++ init/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()                          //connecting to database
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch(err => console.log(err));  
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+};
+
+const initDB = async () => {
+    await Listing.deleteMany({});   //to delete all data from DB
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "68d12765d306c720900e5b5e"}));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+}
+initDB();
+
---
+++ models/review.js
@@ -0,0 +1,23 @@
+// const { types } = require("joi");
+// const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema ({
+    Comment: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    createdAt: {
+        type: Date,
+        dafault: Date.now(),
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    }
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
